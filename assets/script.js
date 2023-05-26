5df5bca3d9f5616f507607d55062eec98e0fcaad
udings <theblackhole1410@gmail.com>
2023-05-26 21:50:46 +0700

    revisi 1

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,27 @@ $(".oth").on("click", function () {
 $(".profile").on("click", function () {
   $(".m-profile").toggle('d-none');
 });
+$(document).ready(function () {
+  new Swiper('.swiper', {
+      loop: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      slidesPerView: 5,
+      paginationClickable: true,
+      spaceBetween: 20,
+      breakpoints: {
+          1920: {
+              slidesPerView: 5,
+              spaceBetween: 30
+          },
+          1028: {
+              slidesPerView: 4,
+              spaceBetween: 30
+          },
+          480: {
+              slidesPerView: 1,
+              spaceBetween: 10
+          }
+      }
+  });
+});
