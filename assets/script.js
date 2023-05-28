29920adbd447de131ff5e559cd273ece105b8d22
Adipebrian <pebrianadi05@gmail.com>
2023-05-28 21:53:38 +0700

    responsive

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,16 @@
 $(".oth").on("click", function () {
-  $(".m-oth").toggle('d-none');
+  $(".m-oth").toggle("d-none");
 });
 $(".profile").on("click", function () {
-  $(".m-profile").toggle('d-none');
+  $(".m-profile").toggle("d-none");
 });
-$(document).ready(function () {
-  new Swiper('.swiper', {
-      loop: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      slidesPerView: 5,
-      paginationClickable: true,
-      spaceBetween: 20,
-      breakpoints: {
-          1920: {
-              slidesPerView: 5,
-              spaceBetween: 30
-          },
-          1028: {
-              slidesPerView: 4,
-              spaceBetween: 30
-          },
-          480: {
-              slidesPerView: 1,
-              spaceBetween: 10
-          }
-      }
-  });
+$(".profile2").on("click", function () {
+  $(".m-profile2").toggle("d-none");
+});
+
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
 });
